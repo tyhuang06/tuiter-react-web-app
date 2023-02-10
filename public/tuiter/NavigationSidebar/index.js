df5af9490d7d153396c7095824a0d6aa1a9110f5
Jamie Huang <tyhuang06@gmail.com>
2023-02-10 12:25:31 -0500

    feat(a5): challenge - need to fix active navigation

--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -1,35 +1,44 @@
-const NavigationSidebar = () => {
+const NavigationSidebar = (active) => {
+	/* console.log(active);
+	$('.list-group-item').removeClass('active');
+
+	if (active === 'home') {
+		$('#wd-home-nav').addClass('active');
+	} else if (active === 'explore') {
+		$('#wd-explore-nav').addClass('active');
+	} */
+
 	return `
     <div class="list-group">
-        <a href="../home.html" class="list-group-item list-group-item-action">
+        <a href="../HomeScreen/index.html" id="wd-home-nav" class="list-group-item list-group-item-action">
             <i class="fa-sharp fa-solid fa-house-chimney"></i>
             <span class="ms-1 d-none d-xl-inline">Home</span>
         </a>
-        <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+        <a href="../ExploreScreen/index.html" id="wd-explore-nav" class="list-group-item list-group-item-action" aria-current="true">
             <i class="fa-regular fa-hashtag"></i>
             <span class="ms-1 d-none d-xl-inline">Explore</span>
         </a>
-        <a href="#" class="list-group-item list-group-item-action">
+        <a href="#" id="wd-notification-nav" class="list-group-item list-group-item-action">
             <i class="fa-regular fa-bell"></i>
             <span class="ms-1 d-none d-xl-inline">Notifications</span>
         </a>
-        <a href="#" class="list-group-item list-group-item-action">
+        <a href="#" id="wd-messages-nav" class="list-group-item list-group-item-action">
             <i class="fa-regular fa-envelope"></i>
             <span class="ms-1 d-none d-xl-inline">Messages</span>
         </a>
-        <a href="../bookmarks/index.html" class="list-group-item list-group-item-action">
+        <a href="../bookmarks/index.html" id="wd-bookmarks-nav" class="list-group-item list-group-item-action">
             <i class="fa-regular fa-bookmark"></i>
             <span class="ms-1 d-none d-xl-inline">Bookmarks</span>
         </a>
-        <a href="#" class="list-group-item list-group-item-action">
+        <a href="#" class="list-group-item id="wd-articles-nav" list-group-item-action">
             <i class="fa-solid fa-fire"></i>
             <span class="ms-1 d-none d-xl-inline">Top Articles</span>
         </a>
-        <a href="../profile.html" class="list-group-item list-group-item-action">
+        <a href="../profile.html" id="wd-profile-nav" class="list-group-item list-group-item-action">
             <i class="fa-regular fa-user"></i>
             <span class="ms-1 d-none d-xl-inline">Profile</span>
         </a>
-        <a href="#" class="list-group-item list-group-item-action">
+        <a href="#" id="wd-more-nav" class="list-group-item list-group-item-action">
             <i class="fa-solid fa-ellipsis"></i>
             <span class="ms-1 d-none d-xl-inline">More</span>
         </a>
