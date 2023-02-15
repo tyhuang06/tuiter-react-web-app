3c27b058502555fd632b6fa241abe8fb57782875
Jamie Huang <tyhuang06@gmail.com>
2023-02-15 14:38:11 -0500

    feat(a6): completed lab

---
+++ src/labs/a6/todo/todo-item.js
@@ -0,0 +1,10 @@
+const TodoItem = ({ todo }) => {
+	return (
+		<li>
+			<input type="checkbox" defaultChecked={todo.done} />
+			{todo.title}({todo.status})
+		</li>
+	);
+};
+
+export default TodoItem;
