16c75141d558755d3e4acf5ea31188e95e5d8c2a
Jamie Huang <tyhuang06@gmail.com>
2023-03-13 12:59:46 -0400

    feat(a7): tuiter: refactored who to follow list

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -4,30 +4,35 @@ import WhoToFollowList from './who-to-follow-list';
 import ExploreComponent from './explore';
 import { Routes, Route } from 'react-router';
 import HomeComponent from './home';
+import { configureStore } from "@reduxjs/toolkit";
+import whoReducer from "./reducers/who-reducer";
+import { Provider } from "react-redux";
+
+const store = configureStore({ reducer: { who: whoReducer } });
 
 function Tuiter() {
-	return (
-		<>
-			<Nav />
-			<div className="row mt-2">
-				<div className="col-2 col-md-2 col-lg-1 col-xl-2">
-					<NavigationSidebar active="explore" />
-				</div>
-				<div
-					className="col-10 col-md-10 col-lg-7 col-xl-6"
-					style={{ position: 'relative' }}
-				>
-					<Routes>
-						<Route path="home" element={<HomeComponent />} />
-						<Route path="explore" element={<ExploreComponent />} />
-					</Routes>
-				</div>
-				<div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-					<WhoToFollowList />
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <Provider store={ store }>
+            <Nav/>
+            <div className="row mt-2">
+                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                    <NavigationSidebar active="explore"/>
+                </div>
+                <div
+                    className="col-10 col-md-10 col-lg-7 col-xl-6"
+                    style={ { position: 'relative' } }
+                >
+                    <Routes>
+                        <Route path="home" element={ <HomeComponent/> }/>
+                        <Route path="explore" element={ <ExploreComponent/> }/>
+                    </Routes>
+                </div>
+                <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                    <WhoToFollowList/>
+                </div>
+            </div>
+        </Provider>
+    );
 }
 
 export default Tuiter;
