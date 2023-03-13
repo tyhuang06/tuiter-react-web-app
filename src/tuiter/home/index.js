a4fb07f031df184b8a73aa1d361ba307a8d8f9b3
Jamie Huang <tyhuang06@gmail.com>
2023-03-13 14:00:23 -0400

    feat(a7): tuiter - create new tuit

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import TuitsList from "../tuits/tuits-list";
+import WhatsHappening from "./whats-happening";
 
 const HomeComponent = () => {
 
     return (
         <>
+            <h4>Home</h4>
+            <WhatsHappening/>
             <TuitsList/>
         </>
     );
--- src/tuiter/reducers/tuits-reducer.js
+++ src/tuiter/reducers/tuits-reducer.js
@@ -1,9 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 import tuitsArray from "../data/tuits.json";
 
+const currentUser = {
+    "userName": "NASA",
+    "handle": "nasa",
+    "image": "nasa.png"
+}
+
+const templateTuit = {
+    ...currentUser,
+    "topic": "Space",
+    "time": "2h",
+    "liked": false,
+    "comments": 0,
+    "retweets": 0,
+    "likes": 0,
+}
+
 const tuitsSlice = createSlice({
     name: "tuits",
     initialState: tuitsArray,
+    reducers: {
+        createTuit: (state, action) => {
+            const newTuit = {
+                ...action.payload,
+                ...templateTuit,
+                "_id": (new Date()).getTime(),
+            }
+            state.unshift(newTuit);
+        }
+    }
 });
 
+export const { createTuit } = tuitsSlice.actions;
 export default tuitsSlice.reducer;
